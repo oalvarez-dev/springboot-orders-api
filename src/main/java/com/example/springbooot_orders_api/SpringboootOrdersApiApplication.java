81d876649cdbddb72f0ab7f78b27946934261ed7
Oliver Alvarez <jess_oliv@hotmail.com>
2025-09-20 18:56:38 -0600

    Initial commit

---
+++ src/main/java/com/example/springbooot_orders_api/SpringboootOrdersApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbooot_orders_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboootOrdersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboootOrdersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbooot_orders_api/SpringboootOrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbooot_orders_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboootOrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
